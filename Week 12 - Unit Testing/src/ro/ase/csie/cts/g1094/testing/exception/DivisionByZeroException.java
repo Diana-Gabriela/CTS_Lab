9980b0e966d18ee7923d9fd3c4377a3bba341c17
User <User@LAPTOP-E4U5V00C>
2021-05-18 15:32:45 +0300

    Final commit j unit test

---
+++ Week 12 - Unit Testing/src/ro/ase/csie/cts/g1094/testing/exception/DivisionByZeroException.java
@@ -0,0 +1,5 @@
+package ro.ase.csie.cts.g1094.testing.exception;
+
+public class DivisionByZeroException extends Exception{
+
+}
---
+++ Week 12 - Unit Testing/src/ro/ase/csie/cts/g1094/testing/main/App.java
@@ -0,0 +1,16 @@
+package ro.ase.csie.cts.g1094.testing.main;
+
+import ro.ase.csie.cts.g1094.testing.models.MathOperations;
+
+public class App {
+
+	public static void main(String[] args) {
+
+
+		System.out.println("App start!");
+		
+		
+		}
+	}
+
+
